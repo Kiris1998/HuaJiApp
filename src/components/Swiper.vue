1102e2b69c0bf1a693f6f2f35a9d08b5a438e993
Kiris_1013 <768049133@qq.com>
2018-08-19 17:30:11 +0800

    finish almost the function

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,6 +1,16 @@
 <template>
   <ul>
-      <li v-for="(item,index) in message" :key="item" :style="{background: colors[index]}">{{item}}</li>
+      <li v-for="(item,index) in message"
+          :key="item"
+          :style="[tansform(index),tansformMain(index)]"
+          @touchstart.stop.capture="touchStart"
+          @touchmove.stop.capture="touchMove"
+          @touchend.stop.capture="touchEnd"
+          @mousedown.stop.capture="touchStart"
+          @mousemove.stop.capture="touchMove"
+          @mouseup.stop.capture="touchEnd"
+          @webkit-transition-end="onTransitionEnd(index)"
+          @transitionend="onTransitionEnd(index)">{{item}}</li>
   </ul>
 </template>
 
@@ -10,10 +20,38 @@ export default {
   data () {
     return {
       tag: 0,
+      position: {
+        start: {
+          x: 0,
+          y: 0
+        },
+        end: {
+          x: 0,
+          y: 0
+        },
+        currentPage: 0
+      },
+      behaviors: {
+        poswidth: '',
+        posheight: '',
+        lastposwidth: '',
+        lastposheight: '',
+        tracking: false,
+        opacity: 1,
+        animate: false,
+        swipe: false
+      },
       message: [
         'Review',
         'Study',
-        'Play'
+        'Play',
+        'Happy',
+        'Delay',
+        'Review3',
+        'Study4',
+        'Play2',
+        'Happy4',
+        'Delay5'
       ],
       colors: [
         '#c2dde6',
@@ -26,6 +64,107 @@ export default {
     }
   },
   methods: {
+    touchStart (e) {
+      if (e.type === 'touchstart') {
+        if (e.touches.length > 1) {
+          this.behaviors.tracking = false
+        } else {
+          this.position.start.t = new Date().getTime
+          this.position.start.x = e.targetTouches[0].clientX
+          this.position.start.y = e.targetTouches[0].clientY
+          this.position.end.x = e.targetTouches[0].clientX
+          this.position.end.y = e.targetTouches[0].clientY
+        }
+      } else {
+        this.position.start.t = new Date().getTime
+        this.position.start.x = e.clientX
+        this.position.start.y = e.clientY
+        this.position.end.x = e.clientX
+        this.position.end.y = e.clientY
+      }
+      this.behaviors.tracking = true
+      this.behaviors.animate = false
+    },
+    touchMove (e) {
+      if (this.behaviors.tracking && !this.behaviors.animate) {
+        if (e.type === 'touchmove') {
+          this.position.end.x = e.targetTouches[0].clientX
+          this.position.end.y = e.targetTouches[0].clientY
+        } else {
+          this.position.end.x = e.clientX
+          this.position.end.y = e.clientY
+        }
+        this.behaviors.poswidth = this.position.end.x - this.position.start.x
+        this.behaviors.posheight = this.position.end.y - this.position.start.y
+      }
+    },
+    touchEnd () {
+      this.behaviors.tracking = false
+      this.behaviors.animate = true
+      if (Math.abs(this.behaviors.poswidth) > 100) {
+        let ratio = Math.abs(this.behaviors.posheight / this.behaviors.poswidth)
+        this.behaviors.poswidth = this.behaviors.poswidth > 0 ? this.behaviors.poswidth + 200 : this.behaviors.poswidth - 200
+        this.behaviors.posheight = this.behaviors.posheight > 0 ? Math.abs(this.behaviors.poswidth * ratio) : -Math.abs(this.behaviors.poswidth * ratio)
+        this.behaviors.opacity = 0
+        this.behaviors.swipe = true
+        this.behaviors.lastposwidth = this.behaviors.poswidth
+        this.behaviors.lastposheight = this.behaviors.posheight
+        this.position.currentPage++
+        this.$nextTick(() => {
+          this.behaviors.poswidth = 0
+          this.behaviors.posheight = 0
+          this.behaviors.opacity = 1
+        })
+      } else {
+        this.behaviors.poswidth = 0
+        this.behaviors.posheight = 0
+        this.behaviors.swipe = false
+      }
+    },
+    onTransitionEnd (index) {
+      if (this.behaviors.swipe && index === this.position.currentPage - 1) {
+        this.behaviors.animate = true
+        this.behaviors.lastposwidth = 0
+        this.behaviors.lastposheight = 0
+        this.behaviors.swipe = false
+      }
+      console.log(index)
+      console.log(this.position.currentPage)
+      console.log(this.position.currentPage === this.message.length - 1)
+    },
+    tansformMain (index) {
+      let style = {}
+      if (index === this.position.currentPage && this.position.currentPage !== this.message.length - 1) {
+        style['transform'] = 'translate3D(' + this.behaviors.poswidth + 'px,' + this.behaviors.posheight + 'px,0px)'
+        style['opacity'] = this.behaviors.opacity
+        style['z-index'] = 10
+        if (this.behaviors.animate) {
+          style['transitionTimingFunction'] = 'ease'
+          style['transitionDuration'] = 300 + 'ms'
+        }
+      }
+      style['background'] = this.colors[index % 6]
+      return style
+    },
+    tansform (index) {
+      if (index > this.position.currentPage && this.position.currentPage !== this.message.length - 1) {
+        let style = {}
+        let pages = 3
+        let myPageNumber = index - this.position.currentPage
+        if (index > this.position.currentPage && myPageNumber < pages) {
+          style['transform'] = 'translate3D(0,0,' + -1 * myPageNumber * 60 + 'px)'
+          style['opacity'] = 1
+          style['z-index'] = myPageNumber * -1 + pages
+        } else {
+          style['z-index'] = -1
+        }
+        style['background'] = this.colors[index % 6]
+        return style
+      } else if (index === this.position.currentPage - 1) {
+        let style = {}
+        return style
+      }
+    }
   }
 }
 </script>
@@ -58,24 +197,5 @@ export default {
     font-weight: bolder;
     font-size: 30px;
     font-family: 'Montserrat', sans-serif;
-  }
-  ul li:nth-child(1){
-    transform: translate3d(0,0,0);
-    z-index:3;
-    background: #c0dfd9;
-  }
-  ul li:nth-child(2){
-    transform: translate3d(20px,-40px,-60px);
-    z-index:2;
-    background: #e9ece9;
-  }
-  ul li:nth-child(3){
-    transform: translate3d(40px,-80px,-120px);
-    z-index:1;
-    background: #b3c2bf;
-  }
-  img{
-    widows: 100%;
-    display: block;
   }
 </style>
